3f3ea0a646d8ec5064f55cda7c7038698647d943
hyen <80624327+hyoie99@users.noreply.github.com>
2022-08-20 18:29:31 +0900

    drawing "smiling person"

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const context = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-context.fillRect(200, 200, 50, 200);
-context.fillRect(400, 200, 50, 200);
-context.lineWidth = 3;
-context.strokeRect(300, 300, 50, 100);
-context.fillRect(200, 200, 200, 20);
+context.fillRect(230, 200, 15, 100);
+context.fillRect(370, 200, 15, 100);
+context.fillRect(280, 200, 60, 200);
 
-context.moveTo(200, 200);
-context.lineTo(325, 100);
-context.lineTo(450, 200);
+context.fillRect(280, 420, 15, 150);
+context.fillRect(325, 420, 15, 150);
+
+context.arc(310, 130, 50, 0, 2 * Math.PI);
+context.fill();
+
+// 새로운 경로 필수
+context.beginPath();
+context.arc(290, 130, 8, Math.PI, 2 * Math.PI);
+context.arc(330, 130, 8, Math.PI, 2 * Math.PI);
+context.fillStyle = "white";
 context.fill();
