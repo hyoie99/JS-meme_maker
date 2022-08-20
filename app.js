cd2b260032b266180b8121dfadde035717775f45
hyen <80624327+hyoie99@users.noreply.github.com>
2022-08-20 18:42:05 +0900

    draw random color line with 'click and move'

--- app.js
+++ app.js
@@ -4,10 +4,32 @@ canvas.width = 800;
 canvas.height = 800;
 
 context.lineWidth = 2;
-context.moveTo(0, 0);
 
-function onClick(event) {
+const colors = [
+  "#c56cf0",
+  "#ffb8b8",
+  "#ff3838",
+  "#ff9f1a",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+  "#18dcff",
+  "#7d5fff",
+  "#4b4b4b",
+];
+
+function onclick(event) {
+  context.beginPath();
+  context.moveTo(event.offsetX, event.offsetY);
+}
+
+function onMove(event) {
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  context.strokeStyle = color;
   context.lineTo(event.offsetX, event.offsetY);
   context.stroke();
 }
-canvas.addEventListener("click", onClick);
+
+canvas.addEventListener("click", onclick);
+canvas.addEventListener("mousemove", onMove);
