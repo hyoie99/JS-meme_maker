b14975c698ebb49b870bbdf8ee456d9cc4202e37
hyen <80624327+hyoie99@users.noreply.github.com>
2022-08-20 18:53:17 +0900

    first drawing board

--- app.js
+++ app.js
@@ -5,31 +5,39 @@ canvas.height = 800;
 
 context.lineWidth = 2;
 
-const colors = [
-  "#c56cf0",
-  "#ffb8b8",
-  "#ff3838",
-  "#ff9f1a",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#7efff5",
-  "#18dcff",
-  "#7d5fff",
-  "#4b4b4b",
-];
+// const colors = [
+//   "#c56cf0",
+//   "#ffb8b8",
+//   "#ff3838",
+//   "#ff9f1a",
+//   "#ff9f1a",
+//   "#fff200",
+//   "#32ff7e",
+//   "#7efff5",
+//   "#18dcff",
+//   "#7d5fff",
+//   "#4b4b4b",
+// ];
 
-function onclick(event) {
-  context.beginPath();
+let isPainting = false;
+
+function onMove(event) {
+  if (isPainting) {
+    context.lineTo(event.offsetX, event.offsetY);
+    context.stroke();
+    return;
+  }
   context.moveTo(event.offsetX, event.offsetY);
 }
 
-function onMove(event) {
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  context.strokeStyle = color;
-  context.lineTo(event.offsetX, event.offsetY);
-  context.stroke();
+function startPainting() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
 }
 
-canvas.addEventListener("click", onclick);
 canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
